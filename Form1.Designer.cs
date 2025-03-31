a5470c599cef049a52cd86a4f07ae47528c0e1bb
aung kaung htet <aungkaunghtetps2100@gmail.com>
2025-03-31 23:48:09 +0700

    i add a label and a text box

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,23 +29,45 @@
         private void InitializeComponent()
         {
             lb01 = new Label();
+            label1 = new Label();
+            txtAungKaungHtet = new TextBox();
             SuspendLayout();
             // 
             // lb01
             // 
             lb01.AutoSize = true;
-            lb01.Location = new Point(288, 43);
+            lb01.Location = new Point(183, 26);
+            lb01.Margin = new Padding(2, 0, 2, 0);
             lb01.Name = "lb01";
-            lb01.Size = new Size(245, 25);
+            lb01.Size = new Size(151, 15);
             lb01.TabIndex = 0;
             lb01.Text = "This is Me Doing Exercise02";
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(115, 87);
+            label1.Name = "label1";
+            label1.Size = new Size(170, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Aung Kaung Htet is doing now";
+            // 
+            // txtAungKaungHtet
+            // 
+            txtAungKaungHtet.Location = new Point(140, 136);
+            txtAungKaungHtet.Name = "txtAungKaungHtet";
+            txtAungKaungHtet.Size = new Size(100, 23);
+            txtAungKaungHtet.TabIndex = 2;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(509, 270);
+            Controls.Add(txtAungKaungHtet);
+            Controls.Add(label1);
             Controls.Add(lb01);
+            Margin = new Padding(2, 2, 2, 2);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -55,5 +77,7 @@
         #endregion
 
         private Label lb01;
+        private Label label1;
+        private TextBox txtAungKaungHtet;
     }
 }
