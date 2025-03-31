2ac76841a0c4ade6f90d37580c35bb72ff668b4f
WYMM433A <waiyanmoemyint2832001@gmail.com>
2025-03-31 23:16:20 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Exercise02
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lb01 = new Label();
+            SuspendLayout();
+            // 
+            // lb01
+            // 
+            lb01.AutoSize = true;
+            lb01.Location = new Point(288, 43);
+            lb01.Name = "lb01";
+            lb01.Size = new Size(245, 25);
+            lb01.TabIndex = 0;
+            lb01.Text = "This is Me Doing Exercise02";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(11F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lb01);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lb01;
+    }
+}
